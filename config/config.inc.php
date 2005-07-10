937dbb2c324536482b17a30777d9435975a90812
spyroux <spyroux@58484943-84fa-0310-a0b1-91dbc3bd8174>
2005-07-10 15:30:59 +0000

    ajout d'un fichier de configuration php

---
+++ config/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+// the path where pear is installed and the path to Obelisk folder
+ini_set('include_path', '/usr/share/php' . PATH_SEPARATOR . '/usr/share/obelisk');
+
+// the database dsn 
+$dsn = "protocol://user:pass@host/dbname";
+
+?>
