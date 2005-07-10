598de8dfef3bcfa0b54facbd8c25f3f22c27999f
spyroux <spyroux@58484943-84fa-0310-a0b1-91dbc3bd8174>
2005-07-10 15:45:15 +0000

    a stupid error....

--- sample/AGIMenu.php
+++ sample/AGIMenu.php
@@ -1,7 +1,7 @@
 <?php
 
 // please modify the path below if needed
-require_once('/usr/share/obelisk');
+require_once('/usr/share/obelisk/obelisk.inc.php');
 
 do_agiMenu(/* put here your menu ID*/ 0);
 
--- sample/WebMenu.php
+++ sample/WebMenu.php
@@ -1,7 +1,7 @@
 <?php
 
 // please modify the path below if needed
-require_once('/usr/share/obelisk');
+require_once('/usr/share/obelisk/obelisk.inc.php');
 
 do_webMenu(/* put here your menu ID*/ 0);
 
