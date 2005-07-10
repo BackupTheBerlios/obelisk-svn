776f7cc9ac2d1c05caf717226aebdcc158f74115
spyroux <spyroux@58484943-84fa-0310-a0b1-91dbc3bd8174>
2005-07-10 20:07:00 +0000

    specification of menu support

---
+++ obelisk/obelisk.inc.php
@@ -0,0 +1,46 @@
+<?php
+
+include('config/config.inc.php');
+
+require_once('DB.php'); // PEAR DB
+
+/* prepare the connection to the database */
+$db = DB::connect($dsn, true); // persistent
+
+function do_webMenu($menuID)
+{
+	sessionStart();
+	
+	// where ?
+	if (!isset($_REQUEST["lastID"]))
+	{	// ouverture du menu passe en parametre
+		
+		// si le module requiert un login : draw_login();
+
+		// sinon dessiner le menu principal
+	}
+	else
+	{
+		$lastID = $_REQUEST["lastID"];
+		// recuperation de l'entree du menu
+
+		// verification des droit d'executions
+
+		// dessiner le path
+
+		// executer l'action
+		////inclure le module lie proprement
+
+		// dessiner le sous menu si il y a lieu sinon 
+
+		// dessiner un lien vers le menu parent
+	}
+}
+
+function do_AGIMenu($menuID)
+{
+	// FIXME: mm chose qu'au dessus mais sort du AGI --> verifier pour 
+	//		la gestion de session
+}
+
+?>
