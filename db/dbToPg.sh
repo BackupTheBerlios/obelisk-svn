e16356ac8b3eb6a9a06b74ca8affba1e453a7be0
spyroux <spyroux@58484943-84fa-0310-a0b1-91dbc3bd8174>
2005-08-28 16:38:41 +0000

    new version with some fix

--- db/dbToPg.sh
+++ db/dbToPg.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 cat db.sql | sed -e 's/INTEGER UNSIGNED NOT NULL AUTO_INCREMENT/SERIAL NOT NULL/' \
+	   | sed -e 's/BIGINT .* AUTO_INCREMENT/BIGSERIAL NOT NULL/' \
+	   | sed -e 's/DATETIME/timestamp/' \
 	   | sed -e 's/UNSIGNED//' \
-	   | sed -e 's/UNIQUE INDEX/UNIQUE/' \
+	   | sed -e 's/UNIQUE INDEX .*(/UNIQUE (/' \
 	   | sed -e 's/^ *INDEX.*$//' \
 	   > db_pg.sql
 
