51070830af578776a1dc735d8a756468266bf9c0
spyroux <spyroux@58484943-84fa-0310-a0b1-91dbc3bd8174>
2005-08-28 16:18:30 +0000

    add a usefull sql script into the svn repsositry

---
+++ db/drop.sql
@@ -0,0 +1,55 @@
+DROP TABLE Iax;
+
+DROP TABLE Sip;
+
+DROP TABLE AgiMenu;
+
+DROP TABLE Price;
+
+DROP TABLE WebMenu;
+
+DROP TABLE Geographical_alias;
+
+DROP TABLE Extension;
+
+DROP TABLE Grp_has_People;
+
+DROP TABLE VoIPAccount;
+
+DROP TABLE AgiInterface;
+
+DROP TABLE Rights;
+
+DROP TABLE NetworkMask;
+
+DROP TABLE Module_Action;
+
+DROP TABLE AgiLog;
+
+DROP TABLE AgiSound_Set;
+
+DROP TABLE People_PrePay_Settings;
+
+DROP TABLE AgiSound;
+
+DROP TABLE Geographical_Group;
+
+DROP TABLE Grp;
+
+DROP TABLE Raw_Extension;
+
+DROP TABLE People;
+
+DROP TABLE WebInterface;
+
+DROP TABLE VoIPChannel;
+
+DROP TABLE Network;
+
+DROP TABLE Module;
+
+DROP TABLE NetworkTimeZone;
+
+DROP TABLE NetworkProvider;
+
+
