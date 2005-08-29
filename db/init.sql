73edc0c1717db6156152432019d42716c4d17545
spyroux <spyroux@58484943-84fa-0310-a0b1-91dbc3bd8174>
2005-08-29 08:08:07 +0000

    little correction

--- db/init.sql
+++ db/init.sql
@@ -2,19 +2,19 @@
 -- think about your dialplan and write it down
 
 -- modules
-insert into modules values ( 1, 'people');
-insert into modules values ( 2, 'geo');
-insert into modules values ( 3, 'outside);
-insert into modules values ( 4, 'menu');
+insert into module values ( 1, 'people');
+insert into module values ( 2, 'geo');
+insert into module values ( 3, 'outside');
+insert into module values ( 4, 'menu');
 
 -- number between 100 and 999 are geographical extension
-insert into extension values ( 100, NULL, 999, 2); 
+insert into extension values ( 100, NULL, 2, 999); 
 
 -- number between 1000 and 9999 are people
-insert into extension values ( 1000, NULL, 9999, 1);
+insert into extension values ( 1000, NULL, 1, 9999);
 
 -- number between 10000 and 9223372036854775807 are outside
-insert into extension values (10000, NULL, 9223372036854775807, 3);
+insert into extension values (10000, NULL, 3, 9223372036854775807);
 
 -- one administrator (set your password here an integer value)
 insert into People values (0, 'admin', 'admin', 'admin', '1234', true, 
